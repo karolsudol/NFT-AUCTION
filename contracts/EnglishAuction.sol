b705e9bbd4ec363a15bc3ff851d57e4cedf441f8
elcomendante <karol@finsane.com>
2022-11-01 13:49:27 +0000

    verifier+

--- contracts/EnglishAuction.sol
+++ contracts/EnglishAuction.sol
@@ -8,6 +8,15 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+
+interface ITest {
+    function isERC1155(address nftAddress) external returns (bool);
+
+    function isERC721(address nftAddress) external returns (bool);
+}
 
 contract EnglishAuction is
     IERC721Receiver,
@@ -17,7 +26,6 @@ contract EnglishAuction is
 {
     struct ListedToken {
         uint256 ID;
-        IERC721 nft;
         address payable seller;
         address payable owner;
         uint256 endAt;
@@ -27,7 +35,9 @@ contract EnglishAuction is
         mapping(address => uint256) bids;
         uint256 bidsCount;
         uint256 price;
+        string currrency;
         address ERC20;
+        address NFT;
         bool listed;
     }
 
@@ -40,15 +50,15 @@ contract EnglishAuction is
     // ****************** VARS ******************
 
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    Counters.Counter private _tokenIDs;
     Counters.Counter private _itemsSold;
-    address payable contractsowner;
+    address payable contractsOwner;
     uint256 adminFee = 0.01 ether;
 
     mapping(uint256 => ListedToken) private idToListedToken;
 
     constructor() ERC721("EnglishAuction", "EAU") {
-        contractsowner = payable(msg.sender);
+        contractsOwner = payable(msg.sender);
     }
 
     // ****************** PUBLIC FUNCTIONS ******************
@@ -68,4 +78,37 @@ contract EnglishAuction is
     ) external pure returns (bytes4) {
         return IERC721Receiver.onERC721Received.selector;
     }
+
+    using ERC165Checker for address;
+    bytes4 public constant IID_ITEST = type(ITest).interfaceId;
+    bytes4 public constant IID_IERC165 = type(IERC165).interfaceId;
+    bytes4 public constant IID_IERC1155 = type(IERC1155).interfaceId;
+    bytes4 public constant IID_IERC721 = type(IERC721).interfaceId;
+
+    function isERC1155(address nftAddress) external view returns (bool) {
+        return nftAddress.supportsInterface(IID_IERC1155);
+    }
+
+    function isERC721(address nftAddress) external view returns (bool) {
+        return nftAddress.supportsInterface(IID_IERC721);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        return interfaceId == IID_ITEST || interfaceId == IID_IERC165;
+    }
+
+    /**
+     *  list on auction NFT that msg.sender has deposited with safeTransferFrom.
+     *  Users willing to list their NFT are free to choose any ERC20 token for bids.
+     *  Also, they have to input the auction start UTC timestamp, auction end UTC timestamp and minimum bid amount.
+     *  During the auction there should be no way for NFT to leave the contract - it should be locked on contract.
+     *  One NFT can participate in only one auction.
+     */
+    function listToken(uint256 _tokenId, uint256 minBid) external {}
 }
